a16de7437fe15f05422acd7d7aa87c0f8aea1e1d
Flavio Amurrio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2023-12-19 00:02:03 -0500

    cibuildwhell

--- src/dev_toolbox/cli/__init__.py
+++ src/dev_toolbox/cli/__init__.py
@@ -1,5 +1,15 @@
 from __future__ import annotations
 
+import time
+
+def fib(n: int) -> int:
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 2) + fib(n - 1)
 
 def dev_toolbox():
+    t0 = time.time()
+    fib(32)
+    print(time.time() - t0)
     print('Hello world!')
