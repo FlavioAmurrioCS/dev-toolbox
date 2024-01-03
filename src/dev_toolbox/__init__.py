5032ba04775e63c7c4117f05b358064c1b9c7b2f
Flavio Amurrio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2024-01-02 22:56:53 -0500

    Add hatch-vcs as a requirement and update versioning strategy

--- src/dev_toolbox/__init__.py
+++ src/dev_toolbox/__init__.py
@@ -1,1 +1,14 @@
 from __future__ import annotations
+
+try:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+except (ImportError, LookupError):
+    try:
+        from dev_toolbox._version import (  # pyright: ignore[reportMissingImports]
+            __version__,  #   # noqa: F401
+        )
+    except ModuleNotFoundError:
+        msg = "dev-toolbox is not correctly installed. " "Please install it with pip."
+        raise RuntimeError(msg)  # noqa: B904, TRY200
