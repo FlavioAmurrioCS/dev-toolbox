d3217342a223e9be1ad3b1eab56d553c9e4c31de
Flavio Amurrio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2024-01-02 22:59:14 -0500

    .

--- src/dev_toolbox/__init__.py
+++ src/dev_toolbox/__init__.py
@@ -6,8 +6,8 @@ try:
     __version__ = get_version(root="..", relative_to=__file__)
 except (ImportError, LookupError):
     try:
-        from dev_toolbox._version import (  # pyright: ignore[reportMissingImports]
-            __version__,  #   # noqa: F401
+        from dev_toolbox._version import (  # type: ignore # noqa: F401
+            __version__,  #
         )
     except ModuleNotFoundError:
         msg = "dev-toolbox is not correctly installed. " "Please install it with pip."
