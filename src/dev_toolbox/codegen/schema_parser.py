a4700e9f7a7b0f9a12ab21ed8ebb127f5b7bc528
Flavio Amurrio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2024-08-25 00:23:57 -0400

    refactor: Improve code readability and maintainability in schema_parser.py

--- src/dev_toolbox/codegen/schema_parser.py
+++ src/dev_toolbox/codegen/schema_parser.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import re
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -89,7 +90,37 @@ def enum_draft_is(value: JsonSchema) -> TypeIs[EnumDraft]:
 ###############################################################################
 
 
-def _parse_draft(*, draft: JsonSchema, property_name: str, lines: list[tuple[str, ...]]) -> str:  # noqa: PLR0911, C901
+def object_draft_parse(
+    *, draft: ObjectDraft, property_name: str, lines: list[tuple[str, ...]]
+) -> str:
+    pattern = re.compile(r"^[a-zA-Z_][a-zA-Z0-9_]*$")
+    has_proper_properties_names = all(pattern.match(name) for name in draft["properties"])
+    clazz_name = property_name.capitalize()
+
+    start = f"class {clazz_name}(TypedDict):"
+    end = ""
+    joiner = ""
+    qoutes = ""
+    if not has_proper_properties_names:
+        start = f'{clazz_name} = TypedDict("{clazz_name}", {{'
+        joiner = ","
+        end = "})"
+        qoutes = '"'
+    buffer = [start]
+    required = set(draft.get("required", []))
+    for name, ztype in draft["properties"].items():
+        type_s = _parse_draft(draft=ztype, property_name=name, lines=lines)
+        if name not in required:
+            type_s = f"NotRequired[{type_s}]"
+        buffer.append(f"    {qoutes}{name}{qoutes}: {type_s}{joiner}")
+
+    buffer.append(end)
+    lines.append(tuple(buffer))
+
+    return clazz_name
+
+
+def _parse_draft(*, draft: JsonSchema, property_name: str, lines: list[tuple[str, ...]]) -> str:  # noqa: PLR0911
     if not draft:
         return "Any"
     if enum_draft_is(draft):
@@ -100,18 +131,7 @@ def _parse_draft(*, draft: JsonSchema, property_name: str, lines: list[tuple[str
     if ref_draft_is(draft):
         return draft["$ref"].rsplit("/", maxsplit=1)[-1]
     if object_draft_is(draft):
-        clazz_name = property_name.capitalize()
-        buffer: list[str] = [
-            f"class {clazz_name}(TypedDict):",
-        ]
-        required = set(draft.get("required", []))
-        for key, value in draft["properties"].items():
-            parsed_type = _parse_draft(draft=value, property_name=key, lines=lines)
-            if key not in required:
-                parsed_type = f"NotRequired[{parsed_type}]"
-            buffer.append(f"    {key}: {parsed_type}")
-        lines.append(tuple(buffer))
-        return clazz_name
+        return object_draft_parse(draft=draft, property_name=property_name, lines=lines)
     if array_draft_is(draft):
         dp: JsonSchema = draft.get("items", {})  # type: ignore
         parsed_type = _parse_draft(draft=dp, property_name=property_name, lines=lines)
@@ -120,7 +140,12 @@ def _parse_draft(*, draft: JsonSchema, property_name: str, lines: list[tuple[str
         types_str = ", ".join(
             _parse_draft(draft=d, property_name=property_name, lines=lines) for d in draft["anyOf"]
         )
-        return f"Union[{types_str}]"
+        types_str = f"Union[{types_str}]"
+        if draft.get("title") is None:
+            return types_str
+        clazz_name: str = draft["title"]  # type: ignore
+        lines.append((f"{clazz_name} = {types_str}",))
+        return f"{clazz_name}"
     if primitive_draft_is(draft):
         return PRIMITIVE_TYPE_CONVERSION[draft["type"]]
     if union_draft_is(draft):
@@ -131,12 +156,11 @@ def _parse_draft(*, draft: JsonSchema, property_name: str, lines: list[tuple[str
     return "Any"
 
 
-def schema_to_typeddicts(schema: JsonSchema) -> str:
+def schema_to_types(schema: JsonSchema) -> str:
     lines: list[tuple[str, ...]] = [
         (
             "from typing import TypedDict, Union, Literal, List",
             "from typing_extensions import NotRequired",
-            "",
             "",
         )
     ]
@@ -194,9 +218,15 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     with open(args.input) as f, open(args.output, "w") as out:
         schema: JsonSchema = json.load(f)
-        out.write(schema_to_typeddicts(schema) + "\n")
+        out.write(schema_to_types(schema) + "\n")
     return 0
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    raise SystemExit(
+        main(
+            [
+                "--input=/Users/flavio/dev/github.com/FlavioAmurrioCS/dev-toolbox/tests/codegen_resources/book_schema.json"
+            ]
+        )
+    )
